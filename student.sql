e700a6d6a9d8874e4f93a103e50048fa1260287f
Bolatito2023 <145867268+Bolatito2023@users.noreply.github.com>
2024-03-16 19:54:34 -0400

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,8 @@
+create table students
+	(student_id		SERIAL PRIMARY KEY, 
+	 first_name	    TEXT NOT NULL, 
+	 last_name		TEXT NOT NULL,
+	 email          TEXT UNIQUE NOT NULL,
+	 enrollment_date DATE
+	 
+	);
